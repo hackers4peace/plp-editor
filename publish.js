08cd7d0ba669920d1718a1a188dbde59fd255719
elf Pavlik <perpetual-tripper@wwelves.org>
2014-09-19 17:32:35 +0200

    fixes just to get it going!!!

--- publish.js
+++ publish.js
@@ -1,30 +1,26 @@
 $(function(){
-  document.getElementById('profile_output').innerHTML = window.localStorage.getItem('profile');
-  console.log(window.localStorage.getItem('profile'));
+  // remoteStorage
   remoteStorage.access.claim('shares', 'rw');
   remoteStorage.displayWidget();
 
-  // Listen for changes
-  //editor.on("change",  function() {
-    //var data = editor.getValue();
-    //remoteStorage.shares.storeFile('application/json-ld', 'me.json', JSON.stringify(data)).then(function(url) {
-      //console.log(url);
-    //});
-  //});
 
-//  $('#publish_btn').click(function() {
-//    var request = require('superagent');
-//    request
-//  .post(window.plp.config.provider)
-//   .send(window.localStorage.getItem('profile'))
-//   .set('Content-Type', 'application/json')
-//  .end(function(res){
-//     if (res.ok) {
-//       alert('yay got ' + JSON.stringify(res.body));
-//     } else {
-//       alert('Oh no! error ' + res.text);
-//     }
-//   });
-// });
+  $('#publish_btn').click(function() {
+  superagent.post(window.plp.config.provider)
+   .send(localStorage.profile)
+   .set('Content-Type', 'application/json')
+    .end(function(provRes){
+       if(provRes.ok) {
+         console.log('yay got ' + JSON.stringify(provRes.body));
+         superagent.post(window.plp.config.directory)
+          .send(provRes.body)
+          .set('Content-Type', 'application/json')
+           .end(function(dirRes){
+             console.log(dirRes.body);
+           });
+       } else {
+         console.log('Oh no! error ' + res.text);
+       }
+     });
+ });
 
 });
